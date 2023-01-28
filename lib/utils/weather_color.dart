bda6e6cc00269debc92bd73a50d9c34f90f4a33d
UmangKaklotar <116021163+UmangKaklotar@users.noreply.github.com>
2023-01-28 16:50:06 +0530

    Update weather_color.dart

--- lib/utils/weather_color.dart
+++ lib/utils/weather_color.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class WeatherColor {
   static Color white = Colors.white;
-  static Color black38 = Colors.black26;
+  static Color black26 = Colors.black26;
   static Color blue = const Color(0xff0D3F67);
 }
